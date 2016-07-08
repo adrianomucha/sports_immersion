62f8488583538f2e50bc3d558226f67ab5a7ba31
Sam Roeca <samuel.roeca@gmail.com>
2016-07-08 03:59:21 -0400

    Revise provisioning script to include build tools for Pandas and NumPy

--- provision.sh
+++ provision.sh
@@ -9,16 +9,19 @@ echo ""
 apt-get update
 
 echo ""
-echo "Linux utils"
+echo "Linux utils (some for Pandas and Numpy if must build from source)"
 echo ""
 apt-get install -y linux-headers-$(uname -r) build-essential git-core
-apt-get install -y libxml2-dev libxslt-dev curl libcurl4-openssl-dev
 apt-get install -y libreadline-dev
+apt-get install -y libxml2-dev libxslt-dev curl libcurl4-openssl-dev \
+  libatlas-base-dev libatlas3gf-base libjpeg-dev libfreetype6-dev libpng-dev
+apt-get install -y gfortran
 
 echo ""
 echo "Python utils"
 echo ""
-apt-get install -y python3 python3-setuptools python3-dev libpq-dev pep8
+apt-get install -y python3 python3-setuptools python3-dev python3-pip \
+  libpq-dev pep8
 
 echo ""
 echo "apt-get cleanup"
@@ -26,24 +29,9 @@ echo ""
 apt-get clean
 
 echo ""
-echo "pip, ipython and virtualenv"
+echo "virtualenv"
 echo ""
-apt-get install -y python3-pip ipython3
 pip3 install virtualenv
-pip3 install virtualenvwrapper
-
-echo ""
-echo ".vimrc"
-echo ""
-cat <<EOF >> ~/.vimrc
-set smartindent
-set tabstop=4
-set shiftwidth=4
-set expandtab
-set number
-syntax on
-colorscheme pablo
-EOF
 
 echo ""
 echo "Disabling firewall"
