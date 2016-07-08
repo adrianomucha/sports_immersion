d8302381ee541ce8b9f6f5f3143147798b1b0685
Sam Roeca <samuel.roeca@gmail.com>
2016-07-08 03:49:08 -0400

    Removed initialization script

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir vagrant-python-env
-cd vagrant-python-env
-
-curl -O https://raw.githubusercontent.com/adionditsak/vagrant-python-env/master/Vagrantfile
-curl -O https://raw.githubusercontent.com/adionditsak/vagrant-python-env/master/provision.sh
-
-vagrant up
