bac72da49a0a9c0174cf6912a1f8d90b14ab0b19
mgoblues <je60@nyu.edu>
2016-08-06 15:51:15 +0000

    updated tokens

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,12 +1,14 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import os
 
 #Variables that contains the user credentials to access Twitter API
-access_token = "760982681198944256-S4jSOYuyItZSxZCifeNKI7KrH7I18S4"
-access_token_secret = "ReEehn5n6bH9QXJNCx9OBs9HIiTOKaALhxLEHW0IjcK3g"
-consumer_key = "i2tMSKimnyjFWnbN6l3fmfCF3"
-consumer_secret = "Bp4uf1aB3YzRyDnUsJ3gquHYLYbLyEd8sEso2f1gYlAxFJYk7Y"
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+
 
 
 #This is a basic listener that just prints received tweets to stdout.
