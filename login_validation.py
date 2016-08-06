b0ad7518c8d24201f1a16db5f2eac8457952ecfb
mgoblues <je60@nyu.edu>
2016-08-06 15:36:04 +0000

    login validation is working

---
+++ login_validation.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient()
+user_logins = client.user_database.user_logins
+
+# # route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    error = None
+    print('start of if statement')
+    if request.method == 'POST':
+        print('this is the login cred')
+        if user_logins.find_one({username: password}) is None:
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            print('Welcome to the site!')
+            return redirect(url_for('welcome'))
+        # print('after login cred')
+        # if login_cred is True:
+        #     return redirect(url_for('welcome'))
+        # else:
+        #     error = 'Invalid Credentials. Please try again.'
+        # if username == 'not a real username':
+        #     pass
+        # else:
+        #     print(username, password)
+        #     print(type(username), type(password))
+
+        #     print(user_logins.insert_one({username: password}))
+        #     print(user_logins.find_one({username: password}))
+        #     print(user_logins.find_one({}))
+        #     return redirect(url_for('welcome'))
+    return render_template('login.html', error=error)
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.config["SECRET_KEY"] = "ITSASECRET"
+    app.run(port=5000, debug=True, host= '0.0.0.0')
