3ba0f517f09952f17e35bb2eb18aee625b89e51a
mgoblues <je60@nyu.edu>
2016-08-08 23:28:18 +0000

    front end linking fixed

--- login_validation.py
+++ login_validation.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
@@ -6,7 +7,7 @@ client = MongoClient()
 user_logins = client.user_database.user_logins
 
 # route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/frontpage', methods=['GET', 'POST'])
 def login():
     '''Tests to see if username is in database. If not found, returns None'''
     username = request.form.get('username')
@@ -26,29 +27,29 @@ def login():
             return redirect(url_for('welcome'))
     return render_template('index.html', error=error)
 
-@app.route('/welcome')
-def welcome():
-    return render_template('register_user.html')  # render a template
+# @app.route('/welcome')
+# def welcome():
+#     return render_template('index.html')  # render a template
 
 #adding users
-@app.route('/add_user', methods=['GET', 'POST'])
-def register():
-    '''Tests to see if username is in database. If not found, returns None'''
-    username = request.form.get('username')
-    password = request.form.get('password')
-    error = None
-    print('start of if statement')
-    if request.method == 'POST':
-        print('this is the login cred')
-        if user_logins.find_one({username: password}) is None:
-            user_logins.insert_one({username: password})
-            print('New Login added')
-            # check to ensure new login was added to database
-            if user_logins.find_one({username: password}) is None:
-                error = 'Database error, try again'
-        elif user_logins.insert_one({username: password}) != None:
-            error = 'Username already exists, try again'
-    return render_template('register_user.html', error=error)
+# @app.route('/add_user', methods=['GET', 'POST'])
+# def register():
+#     '''Tests to see if username is in database. If not found, returns None'''
+#     username = request.form.get('username')
+#     password = request.form.get('password')
+#     error = None
+#     print('start of if statement')
+#     if request.method == 'POST':
+#         print('this is the login cred')
+#         if user_logins.find_one({username: password}) is None:
+#             user_logins.insert_one({username: password})
+#             print('New Login added')
+#             # check to ensure new login was added to database
+#             if user_logins.find_one({username: password}) is None:
+#                 error = 'Database error, try again'
+#         elif user_logins.insert_one({username: password}) != None:
+#             error = 'Username already exists, try again'
+#     return render_template('index.html', error=error)
 
 
 #start the server with the 'run()' method
